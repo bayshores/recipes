3a6158f0bf0b3f5f19836593b063f54ff33db4cd
Satish Kamatkar <skamatkar@illumina.com>
2018-03-07 18:07:13 -0800

    Communication between components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -10,13 +10,18 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Blueberry Muffin', 'Delicious sweet treat',
       'http://www.onceuponachef.com/images/2014/07/Best-Blueberry-Muffins2-1024x660.jpg'),
-    new Recipe('Blueberry Muffin', 'Delicious sweet treat',
-      'http://www.onceuponachef.com/images/2014/07/Best-Blueberry-Muffins2-1024x660.jpg')
+    new Recipe('Apple Pie', 'Awesome treat',
+      'https://cbsla.files.wordpress.com/2013/09/apple-pie-thinkstock.jpg')
   ];
 
+  @Output() recipeItemSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeItemSelected.emit(recipe);
+  }
 }
